f49bfdf4a05617618378994cc9440cc57c390d00
Rebecca Gordon <14rebeccagordon@gmail.com>
2022-08-10 16:18:55 -0700

    Calculator functions

--- calculator.py
+++ calculator.py
@@ -4,4 +4,42 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+while True:
+    equation = input("Equation? ")
+    tokens = equation.split(" ")
+    operand = tokens[0]
+
+    if "q" in tokens:
+        print("Bye!")
+        break
+
+    num1 = tokens[1]
+
+    if len(tokens) > 2:
+        num2 = tokens[2]
+
+    elif operand == "+":
+        result = add(float(num1),float(num2))
+    
+    elif operand == "-":
+        result = subtract(float(num1),float(num2))
+    
+    elif operand == "*":
+        result = multiply(float(num1),float(num2))
+    
+    elif operand == "/":
+        result = divide(float(num1),float(num2))
+    
+    elif operand == "square":
+        result = square(float(num1))
+
+    elif operand == "cube":
+        result = cube(float(num1))
+    
+    elif operand == "pow":
+        result = power(float(num1),float(num2))
+    
+    elif operand == "mod":
+        result = mod(float(num1),float(num2))
+    print(result)
+
